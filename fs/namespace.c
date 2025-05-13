a7c02f14cc7103393932d0610d5720e72695653f
Christoph Hellwig <hch@lst.de>
2025-05-13 20:17:43 -0300

    fs: fix a struct path leak in path_umount Make sure we also put the dentry and vfsmnt in the illegal flags and !may_umount cases.
    Fixes: 41525f5 ("fs: refactor ksys_umount")
    Reported-by: Vikas Kumar <vikas.kumar2@arm.com>
    Change-Id: I1e199be072a90a3295c31566399a3d3f26f896b9
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1942,10 +1942,9 @@ static inline bool may_mandlock(void)
 #include <linux/io_record.h>
 #endif
 
-static int path_umount(struct path *path, int flags)
+static int can_umount(const struct path *path, int flags)
 {
-	struct mount *mnt;
-	int retval;
+	struct mount *mnt = real_mount(path->mnt);
 
 	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
 		return -EINVAL;
@@ -1956,30 +1955,37 @@ static int path_umount(struct path *path, int flags)
 #ifdef CONFIG_PAGE_BOOST_RECORDING
 	forced_init_record();
 
-	mnt = real_mount(path->mnt);
-	retval = -EINVAL;
 	if (path->dentry != path->mnt->mnt_root)
-		goto dput_and_out;
+		return -EINVAL;
+
 	if (!check_mnt(mnt))
-		goto dput_and_out;
+		return -EINVAL;
 #ifdef CONFIG_KDP_NS
 	if (((struct kdp_mount *)mnt)->mnt->mnt_flags & MNT_LOCKED)
 #else
 	if (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */
+		return -EINVAL;
 #endif
-		goto dput_and_out;
-	retval = -EPERM;
 	if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
-		goto dput_and_out;
+		return -EPERM;
+	return 0;
+}
 
-	retval = do_umount(mnt, flags);
-dput_and_out:
+int path_umount(struct path *path, int flags)
+{
+	struct mount *mnt = real_mount(path->mnt);
+	int ret;
+
+	ret = can_umount(path, flags);
+	if (!ret)
+		ret = do_umount(mnt, flags);
+
 	/* we mustn't call path_put() as that would clear mnt_expiry_mark */
 	dput(path->dentry);
 	mntput_no_expire(mnt);
 	if (!retval)
 		sys_umount_trace_print(mnt, flags);
-	return retval;
+	return ret;
 }
 
 int ksys_umount(char __user *name, int flags)
